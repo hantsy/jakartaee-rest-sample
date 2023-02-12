42ea428200fa3a30c9a5bdbfa48d08b9757751d2
hantsy <hantsy@gmail.com>
2023-02-12 15:32:26 +0800

    chore: clean codes

--- src/test/java/com/example/it/TaskRepositoryTest.java
+++ src/test/java/com/example/it/TaskRepositoryTest.java
@@ -1,40 +1,41 @@
 package com.example.it;
 
 import com.example.domain.common.AbstractEntity;
-import com.example.domain.task.TaskRepository;
 import com.example.domain.task.Task;
+import com.example.domain.task.TaskRepository;
 import com.example.infrastructure.persistence.jpa.AbstractRepository;
 import com.example.infrastructure.persistence.jpa.JpaTaskRepository;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Arquillian.class)
+
+@ExtendWith(ArquillianExtension.class)
 public class TaskRepositoryTest {
     private static final Logger LOGGER = Logger.getLogger(TaskRepositoryTest.class.getName());
 
     @Deployment()
     public static JavaArchive createDeployment() {
         JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
-                .addPackage(AbstractEntity.class.getPackage())
-                .addPackage(Task.class.getPackage())
-                .addClass(JpaTaskRepository.class).addClass(AbstractRepository.class)
-                //Add JPA persistence configuration.
-                //WARN: In a jar archive, persistence.xml should be put into /META-INF
-                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml");
+            .addPackage(AbstractEntity.class.getPackage())
+            .addPackage(Task.class.getPackage())
+            .addClass(JpaTaskRepository.class).addClass(AbstractRepository.class)
+            //Add JPA persistence configuration.
+            //WARN: In a jar archive, persistence.xml should be put into /META-INF
+            .addAsManifestResource("META-INF/persistence.xml", "persistence.xml");
 
         LOGGER.log(Level.INFO, "deployment unit: {0}", jar);
 
@@ -49,21 +50,21 @@ public class TaskRepositoryTest {
 
     Task saved;
 
-    @Before
+    @BeforeEach
     public void setup() {
         saved = tasks.save(Task.of("test task", "desc of test task"));
     }
 
-    @After
+    @AfterEach
     public void teardown() {
-
+        tasks.delete(saved);
     }
 
     @Test
     public void shouldCreated() {
         Task found = em.find(Task.class, saved.getId());
 
-        assertEquals("name is 'test task'", "test task", found.getName());
-        assertEquals("description is 'desc of test task'", "desc of test task", found.getDescription());
+        assertEquals("test task", found.getName(), "name is 'test task'");
+        assertEquals("desc of test task", found.getDescription(), "description is 'desc of test task'");
     }
 }
--- src/test/java/com/example/it/TaskResourceTest.java
+++ src/test/java/com/example/it/TaskResourceTest.java
@@ -3,101 +3,101 @@ package com.example.it;
 import com.example.application.util.SampleDataPopulator;
 import com.example.domain.common.AbstractEntity;
 import com.example.domain.task.Task;
-import com.example.domain.user.User;
 import com.example.infrastructure.persistence.jpa.AbstractRepository;
 import com.example.infrastructure.persistence.jpa.JpaTaskRepository;
 import com.example.interfaces.RestConfiguration;
 import com.example.interfaces.common.PageParam;
 import com.example.interfaces.common.PagedResult;
 import com.example.interfaces.task.TaskResources;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(Arquillian.class)
+
+@ExtendWith(ArquillianExtension.class)
 public class TaskResourceTest {
     private static final Logger LOGGER = Logger.getLogger(TaskResourceTest.class.getName());
-    
+
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class)
-                
-                // entities
-                .addPackage(AbstractEntity.class.getPackage())
-                .addPackage(Task.class.getPackage())
-                .addPackage(User.class.getPackage())
-                .addClass(JpaTaskRepository.class).addClass(JpaUserRepository.class).addClass(AbstractRepository.class)
-                
-                //sample data
-                .addPackages(true, SampleDataPopulator.class.getPackage())
-                
-                // rest
-                .addPackage(TaskResources.class.getPackage())
-                .addPackage(PageParam.class.getPackage())
-        
-                //rest config
-                .addClass(RestConfiguration.class)
-                
-                //Add JPA persistence configuration.
-                //WARN: In a war archive, persistence.xml should be put into /WEB-INF/classes/META-INF/, not /META-INF
-                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-                // Enable CDI
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-        
+
+            // entities
+            .addPackage(AbstractEntity.class.getPackage())
+            .addPackage(Task.class.getPackage())
+            .addClass(JpaTaskRepository.class).addClass(AbstractRepository.class)
+
+            //sample data
+            .addPackages(true, SampleDataPopulator.class.getPackage())
+
+            // rest
+            .addPackage(TaskResources.class.getPackage())
+            .addPackage(PageParam.class.getPackage())
+
+            //rest config
+            .addClass(RestConfiguration.class)
+
+            //Add JPA persistence configuration.
+            //WARN: In a war archive, persistence.xml should be put into /WEB-INF/classes/META-INF/, not /META-INF
+            .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+            // Enable CDI
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+
         LOGGER.log(Level.INFO, "deployment unit: {0}", war);
-        
+
         return war;
     }
-    
+
     @ArquillianResource
     private URL base;
-    
+
     private Client client;
-    
-    @Before
+
+    @BeforeEach
     public void setup() {
         this.client = ClientBuilder.newClient();
     }
-    
-    @After
+
+    @AfterEach
     public void teardown() {
         if (this.client != null) {
             this.client.close();
         }
     }
-    
+
     @Test
     public void shouldFoundTasks() throws MalformedURLException {
         final WebTarget getAllTasksTarget = client.target(URI.create(new URL(base, "api/tasks").toExternalForm()));
         try (final Response getAllTasksResponse = getAllTasksTarget.request()
-                .accept(MediaType.APPLICATION_JSON)
-                .get()) {
-            assertTrue("status is ok", getAllTasksResponse.getStatus() == 200);
-            assertTrue("response should contain two tasks",
-                    getAllTasksResponse.readEntity(new GenericType<PagedResult<Task>>() {
-                    }).getContent().size() == 2);
-            
+            .accept(MediaType.APPLICATION_JSON)
+            .get()) {
+            assertTrue(getAllTasksResponse.getStatus() == 200, "status is ok");
+            assertTrue(getAllTasksResponse.readEntity(new GenericType<PagedResult<Task>>() {
+                }).getContent().size() == 2,
+                "response should contain two tasks"
+            );
+
         }
     }
 }
